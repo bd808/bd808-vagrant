16c73fa7c4ba5591a9a121e045364c69dccd5001
Bryan Davis <bd808@bd808.com>
2020-08-01 16:41:18 -0600

    phpdev: drop versions < 7.0.32

--- puppet/modules/role/manifests/phpdev.pp
+++ puppet/modules/role/manifests/phpdev.pp
@@ -11,38 +11,39 @@ class role::phpdev {
         'valgrind',
     ]: }
 
-   phpenv::install { '5.5.38': }
-   phpenv::install { '5.5.38-debug':
-       version => '5.5.38',
-       debug   => true,
-   }
-   phpenv::install { '5.5.38-zts':
-       version => '5.5.38',
-       zts     => true,
-   }
-   phpenv::install { '5.5.38-debug-zts':
-       version => '5.5.38',
-       debug   => true,
-       zts     => true,
-   }
-
-   phpenv::install { '5.6.38': }
-   phpenv::install { '5.6.38-debug':
-       version => '5.6.38',
-       debug   => true,
-   }
-   phpenv::install { '5.6.38-zts':
-       version => '5.6.38',
-       zts     => true,
-   }
-   phpenv::install { '5.6.38-debug-zts':
-       version => '5.6.38',
-       zts     => true,
-       debug   => true,
-   }
+#   phpenv::install { '5.5.38': }
+#   phpenv::install { '5.5.38-debug':
+#       version => '5.5.38',
+#       debug   => true,
+#   }
+#   phpenv::install { '5.5.38-zts':
+#       version => '5.5.38',
+#       zts     => true,
+#   }
+#   phpenv::install { '5.5.38-debug-zts':
+#       version => '5.5.38',
+#       debug   => true,
+#       zts     => true,
+#   }
+#
+#   phpenv::install { '5.6.38': }
+#   phpenv::install { '5.6.38-debug':
+#       version => '5.6.38',
+#       debug   => true,
+#   }
+#   phpenv::install { '5.6.38-zts':
+#       version => '5.6.38',
+#       zts     => true,
+#   }
+#   phpenv::install { '5.6.38-debug-zts':
+#       version => '5.6.38',
+#       zts     => true,
+#       debug   => true,
+#   }
 
     phpenv::install { '7.0.32': }
     phpenv::install { '7.1.24': }
     phpenv::install { '7.2.12': }
-    phpenv::install { '7.3.0RC5': }
+    phpenv::install { '7.3.17': }
+    phpenv::install { '7.4.5': }
 }
